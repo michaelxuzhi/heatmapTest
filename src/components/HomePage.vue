3c5d4a2fac2a187573a5adaf58cf3976b0a9bf5f
michaelxuzhi <michaelxuzhi@foxmail.com>
2023-05-23 00:45:32 +0800

    feat: 新增上传表格文件功能

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -12,10 +12,20 @@
             <img id="bg_img" :src="coverImgUrl ? coverImgUrl : baseImg" alt="背景图" />
         </div>
     </div>
+
+    <button @click="uploadExcel">上传文件</button>
+    <input
+        type="file"
+        name="file"
+        id="excelFile"
+        @change="getExcelFile($event)"
+        style="display: none"
+    />
 </template>
 
 <script>
 import h337 from 'heatmap.js';
+import * as XLSX from 'xlsx';
 export default {
     name: 'HomePage',
     data() {
@@ -24,6 +34,7 @@ export default {
             pointsArr: [{ x: 115, y: 115, value: 1000 }],
             coverImgUrl: require('../assets/1.png'),
             baseImg: '',
+            dataFormatFromExcel: {},
         };
     },
     mounted() {
@@ -32,7 +43,7 @@ export default {
         };
         // this.drawMap();
         console.log('do_mounted');
-        this.initHeatMapOrigin();
+        // this.initHeatMapOrigin();
     },
     created() {},
     methods: {
@@ -40,6 +51,10 @@ export default {
         // 上传本地图片
         uploadImg() {
             document.querySelector('#file').click();
+        },
+        // 上传excel文件
+        uploadExcel() {
+            document.querySelector('#excelFile').click();
         },
         // 展示本地图片
         getPicture(e) {
@@ -61,7 +76,7 @@ export default {
                 //使用axios的post方法上传即可
                 console.log(e, '图片上传成功');
                 this.setMapTestSize();
-                this.clearMap();
+                // this.clearMap();
             };
             reader.readAsDataURL(e.target.files[0]);
         },
@@ -75,6 +90,42 @@ export default {
             // 赋值
             document.querySelector('#map_test').style.width = imgW + 'px';
             document.querySelector('#map_test').style.height = imgH + 'px';
+        },
+
+        // 读取上传的excel文件
+        getExcelFile(e) {
+            if (!e.target.files[0]) return; //选择了取消
+            let name = e.target.files[0].name; //读取选中文件的文件名
+            let size = e.target.files[0].size; //读取选中文件的大小
+            console.log(name, size);
+
+            const fileReader = new FileReader();
+            fileReader.readAsBinaryString(e.target.files[0]);
+            fileReader.onload = ev => {
+                try {
+                    const data = ev.target.result;
+                    const workbook = XLSX.read(data, { type: 'binary' });
+                    const wsname = workbook.SheetNames[0];
+                    const ws = XLSX.utils.sheet_to_json(workbook.Sheets[wsname]);
+                    for (let i = 0; i < ws.length; i++) {
+                        let rawLog = ws[i].raw;
+                        let splitArr = rawLog.split('[<em>TouchTapLog</em>],');
+                        let mainInfo = JSON.parse(splitArr[1]);
+                        this.dataFormatFromExcel[i] = {};
+                        this.dataFormatFromExcel[i]['ScreenX'] = mainInfo['stageW'];
+                        this.dataFormatFromExcel[i]['ScreenY'] = mainInfo['stageH'];
+                        this.dataFormatFromExcel[i]['trueX'] = parseInt(
+                            mainInfo['coord'][0] * mainInfo['stageW']
+                        );
+                        this.dataFormatFromExcel[i]['trueY'] = parseInt(
+                            mainInfo['coord'][1] * mainInfo['stageH']
+                        );
+                    }
+                    this.initHeatMapOrigin();
+                } catch (error) {
+                    console.error(error);
+                }
+            };
         },
 
         initHeatMapOrigin() {
@@ -97,15 +148,28 @@ export default {
             // create heatmap with configuration
             this.heapMapIns = h337.create(config);
 
-            // a single datapoint
-            let point = {
-                x: 100, // x coordinate of the datapoint, number
-                y: 100, // y coordinate of the datapoint, number
-                value: 1, // the value at datapoint(x, y)
-            };
-            let dataPoint = point;
-            this.heapMapIns.addData(dataPoint);
-
+            // a single datapoint ---旧版
+            // let point = {
+            //     x: 100, // x coordinate of the datapoint, number
+            //     y: 100, // y coordinate of the datapoint, number
+            //     value: 1, // the value at datapoint(x, y)
+            // };
+            // let dataPoint = point;
+            // this.heapMapIns.addData(dataPoint);
+            // -----------------------------
+            // -------------------新版
+            let points = [];
+            let data = {};
+            for (let k in this.dataFormatFromExcel) {
+                data['x'] = this.dataFormatFromExcel[k].trueX;
+                data['y'] = this.dataFormatFromExcel[k].trueY;
+                data['value'] = 1;
+                points.push(data);
+                data = {};
+            }
+            console.log(points);
+            this.heapMapIns.setData({ max: 5, min: 1, data: points });
+            // ---------------------
             // multiple datapoint
             // (for data initialization use setData!!)
             // this.heapMapIns.addData(this.pointsArr);
@@ -113,7 +177,7 @@ export default {
         // 清除已有热力图
         clearMap() {
             let data = {
-                max: 100,
+                max: 0,
                 min: 0,
                 data: [],
             };
